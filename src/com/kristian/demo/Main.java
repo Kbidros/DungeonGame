8e5522701b702db5878dfd5a72cfbf0beb804f10
Kbidros <kristianbidros@gmail.com>
2023-10-29 21:54:54 +0100

    The basis of the game

--- src/com/kristian/demo/Main.java
+++ src/com/kristian/demo/Main.java
@@ -1,5 +1,57 @@
 package com.kristian.demo;
 
+import java.util.Scanner;
+
+import static com.kristian.demo.Colors.*;
+
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Instantiate Objects
+        Player player = new Player();
+
+
+        System.out.println(GREEN + "Welcome Adventurer" + RESET);
+        System.out.println(BLUE + "What is your name?" + RESET);
+        player.setName(sc.nextLine());
+
+        System.out.println("Welcome to the game " + player.getName() + "," + " what would you like to do?");
+
+        // Menu
+
+        do {
+
+            System.out.println("1.Fight\n2.Status\n3.Exit Game");
+            switch (sc.nextLine()) {
+                case "1" -> fightMenu(player);
+                case "2" -> player.getStatus();
+                case "3" -> System.exit(0);
+
+                default -> System.out.println("Try again!");
+            }
+        }while (true);
+
+    }
+
+    public static void fightMenu (Player player) {
+
+        System.out.println("Inside another MENU");
+        switch (sc.nextLine()) {
+            case "1" -> System.out.println("Player " + player.getName() + " is fighting");
+            case "2" -> System.out.println("Number #2");
+
+            default -> System.out.println("Try again");
+
+
+        }
+
+
+
+    }
+
+
 
 }
